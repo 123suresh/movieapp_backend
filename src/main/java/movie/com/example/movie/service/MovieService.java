50813d31358d0220f3f462275c05005c4679cbcf
sthapa <sureshthapa409@gmail.com>
2024-08-26 10:40:46 -0400

    Update movie controller added

--- src/main/java/movie/com/example/movie/service/MovieService.java
+++ src/main/java/movie/com/example/movie/service/MovieService.java
@@ -2,10 +2,13 @@ package movie.com.example.movie.service;
 
 import movie.com.example.movie.dto.MovieListReq;
 import movie.com.example.movie.dto.MovieListRes;
+import org.bson.types.ObjectId;
 
 import java.util.List;
 
 public interface MovieService {
     public void saveMoviesList(List<MovieListReq> movieListReq);
     public List<MovieListRes> getAllMovie();
+    public List<MovieListRes> getMovieById(ObjectId movieId);
+    public String updateMovieById (ObjectId movieId, MovieListReq MovieListReq);
 }
--- src/main/java/movie/com/example/movie/service/MovieServiceImpl.java
+++ src/main/java/movie/com/example/movie/service/MovieServiceImpl.java
@@ -1,20 +1,23 @@
 package movie.com.example.movie.service;
 
+import lombok.extern.slf4j.Slf4j;
 import movie.com.example.movie.dto.MovieListRes;
 import movie.com.example.movie.repository.MovieRepository;
 import movie.com.example.movie.dto.MovieListReq;
 import movie.com.example.movie.entity.MovieList;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 @Service
 public class MovieServiceImpl implements MovieService{
     @Autowired
     private MovieRepository movieRepository;
-//    public List<MovieListRes> saveMoviesList(List<MovieListReq> movieListReq) {
       public void saveMoviesList(List<MovieListReq> movieListReq) {
           List<MovieList> savedMovieList= movieRepository.saveAll(movieListReq.stream().map(movie->MovieList.builder()
                           .title(movie.getTitle())
@@ -31,7 +34,6 @@ public class MovieServiceImpl implements MovieService{
     }
 
     public List<MovieListRes> getAllMovie() {
-//          List<MovieList> movieList = movieRepository.findAll();
           List<MovieListRes> finalMovieListRes=movieRepository.findAll().stream().map(movie->MovieListRes.builder()
                         .id(movie.getId())
                         .title(movie.getTitle())
@@ -46,5 +48,42 @@ public class MovieServiceImpl implements MovieService{
                 ).toList();
         return finalMovieListRes;
     }
+
+    public List<MovieListRes> getMovieById(ObjectId movieId){
+        List<MovieListRes> movieById=movieRepository.findById(movieId).stream().map(movie->MovieListRes.builder()
+                .id(movie.getId())
+                .title(movie.getTitle())
+                .disc(movie.getDisc())
+                .src(movie.getSrc())
+                .cast1(movie.getCast1())
+                .cast1Name(movie.getCast1Name())
+                .cast2(movie.getCast2())
+                .cast2Name(movie.getCast2Name())
+                .videourl(movie.getVideourl())
+                .build()
+        ).toList();
+        return movieById;
+    }
+
+    public String updateMovieById (ObjectId movieId, MovieListReq updateReq){
+          Optional<MovieList> movie = movieRepository.findById(movieId);
+          if(movie.isEmpty()){
+              log.error("Movie not found with Id ", movieId);
+              return "Movie not found with given Id";
+          }
+          MovieList updateMovie = movie.get();
+          updateMovie.setTitle(updateReq.getTitle()!=null?updateReq.getTitle():updateMovie.getTitle());
+          updateMovie.setDisc(updateReq.getDisc()!=null?updateReq.getDisc():updateMovie.getDisc());
+          updateMovie.setSrc(updateReq.getSrc()!=null?updateReq.getSrc():updateMovie.getSrc());
+          updateMovie.setCast1(updateReq.getCast1()!=null?updateReq.getCast1():updateMovie.getCast1());
+          updateMovie.setCast1Name(updateReq.getCast1Name()!=null?updateReq.getCast1Name():updateMovie.getCast1Name());
+          updateMovie.setCast2(updateReq.getCast2()!=null?updateReq.getCast2():updateMovie.getCast2());
+          updateMovie.setCast2Name(updateReq.getCast2Name()!=null?updateReq.getCast2Name():updateMovie.getCast2Name());
+          updateMovie.setVideourl(updateReq.getVideourl()!=null?updateReq.getVideourl():updateMovie.getVideourl());
+          movieRepository.save(updateMovie);
+          return "Movie "+updateMovie.getTitle()+" updated successfully";
+    }
+
 }
+
 
